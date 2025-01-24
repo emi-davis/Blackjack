5df1843ea618de93566bc524bbda33b50e5ffed8
Emilia <EmiliaDavis2027@gmail.com>
2025-01-24 12:33:11 -0600

    added input checking

--- main.py
+++ main.py
@@ -4,7 +4,15 @@
 from deck import Deck
 
 def main():
-    num_decks = int(input("How many decks would you like to use? "))
+    while True:
+        try:
+            num_decks = int(input("How many decks would you like to use? (1-6) "))
+            if 1 <= num_decks <= 6:
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Please enter a valid number between 1-6.")
     deck = Deck(num_decks)
     deck.printDeck()
 
